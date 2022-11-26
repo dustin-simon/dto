70f155a6f51c3c698b1d9fe34d72d0349fd27701
Dustin <dustin.simon@8mylez.com>
2022-11-26 21:47:05 +0100

    Base classes

---
+++ src/AbstractDto.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Dustin\Dto;
+
+abstract class AbstractDto implements DtoInterface
+{
+    use DtoTrait;
+
+    use ArrayAccessTrait;
+
+    abstract public function __construct(array $data = []);
+
+    public function getIterator(): \Traversable
+    {
+        yield from $this->toArray();
+    }
+
+    public function isEmpty(): bool
+    {
+        return empty($this->toArray());
+    }
+
+    public function __clone()
+    {
+        foreach ($this->toArray() as $field => $value) {
+            if (is_object($value)) {
+                $this->set($field, clone $value);
+            }
+        }
+    }
+}
---
+++ src/NestedDto.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dustin\Dto;
+
+/**
+ * Encapsulation class which can only hold scalar values, arrays or other NestedEncapsulations.
+ */
+class NestedDto extends Dto
+{
+    public function set(string $field, $value): void
+    {
+        $this->validate($value);
+
+        parent::set($field, $value);
+    }
+
+    private function validate($value)
+    {
+        if (
+            $value === null ||
+            \is_scalar($value)
+        ) {
+            return;
+        }
+
+        if (\is_object($value) &&
+            $value instanceof self
+        ) {
+            return;
+        }
+
+        if (is_array($value)) {
+            foreach ($value as $valueItem) {
+                $this->validate($valueItem);
+            }
+
+            return;
+        }
+
+        $type = is_object($value) ? get_class($value) : gettype($value);
+
+        throw new \InvalidArgumentException(\sprintf('A NestedDto can only contain scalar values, null, arrays or other NestedDtos. %s given.', $type));
+    }
+}
